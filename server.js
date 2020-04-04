bac7bf5b9ef99eeaf1ffdf4dc29004eb1dc81018
henrique <hemagso@gmail.com>
2020-04-04 18:21:53 -0400

    Added POST API endpoint to collect data

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+
+app.use(bodyParser.json({ extended: true }));
+
+app.get('/hey', (req, res) => res.send("{test: 12}"))
+app.post('/api/store', (req, res) => {
+    console.log("Got new item", req.body)
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        let dbo = db.db("covid");
+          dbo.collection("covid").insertOne(req.body, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+          });
+    });
+    res.sendStatus(200)
+});
+
+app.listen(8080)
+console.log("Listening on port 8080")
