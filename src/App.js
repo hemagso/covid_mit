406600d4e07d898d9a88bde8e93e90bd35b4390a
henrique <hemagso@gmail.com>
2020-04-04 18:21:30 -0400

    Added logic to POST the collected data

--- src/App.js
+++ src/App.js
@@ -2,46 +2,75 @@ import React, {Component} from 'react';
 import * as Survey from 'survey-react';
 import 'survey-react/survey.css'
 import './App.css';
+import axios from 'axios'
+import {survey_params, survey_version} from "./mock_survey";
 
 Survey.StylesManager.applyTheme("default")
 
 class App extends Component {
-    json = {
-        elements: [
-            { type: "text", name: "customerName", title: "What is your name?", isRequired: true},
-            { type: "text", name: "customerLastName", title: "What is your last name?", isRequired: true},
-            { type: "boolean", name: "locationAuthorization", title: "Share my location data", labelTrue: "Yes", labelFalse: "No", defaultValue: "false"}
-        ]
-    };
+ json = survey_params;
 
-    onComplete(survey, options) {
-        if(survey.data.locationAuthorization) {
-             navigator.geolocation.getCurrentPosition(
-                function(position) {
-                    alert("Current Location = " + String(position.coords.latitude) + ", " + String(position.coords.longitude));
-                },
-                function(error) {
-                    alert(error.message);
-                },
-                {
-                    enableHighAccuracy: true,
-                    timeout: 5000
-                }
-            );
-        }
-        alert("Survey Results = " + JSON.stringify(survey.data));
-    }
+ submit = function(data) {
+  axios.post(
+      "/api/store",
+      JSON.stringify(data),
+      {headers:{ 'Content-Type': 'application/json' }}
+  );
+ };
 
-    render() {
-        let model = new Survey.Model(this.json)
+ onComplete = (survey, options) => {
+  if(survey.data.locationAuthorization) {
+   navigator.geolocation.getCurrentPosition(
+       (position) => {
+        let {latitude, longitude} = position.coords;
+        this.submit({
+         ...survey.data,
+         version: survey_version,
+         location: {
+          status: "success",
+          data: {
+           latitude: latitude,
+           longitude: longitude
+          }
+         }
+        });
+       },
+       (error) => {
+        survey.data.location = {
+         status: "fail"
+        };
+        this.submit({
+         ...survey.data,
+         version: survey_version,
+         location: {
+          status: "fail"
+         }
+        });
+       },
+       {
+        enableHighAccuracy: true,
+        timeout: 5000
+       }
+   );
+  }
+  else {
+   this.submit({
+    ...survey.data,
+    version: survey_version
+   });
+  }
+ };
 
-        return(
-            <div>
-                <Survey.Survey model={model} onComplete={this.onComplete}/>
-            </div>
-        );
+ render() {
+  let model = new Survey.Model(this.json)
 
-    }
+  return(
+      <div>
+       <Survey.Survey model={model} onComplete={this.onComplete}/>
+      </div>
+  );
+
+ }
 }
 
 export default App;
