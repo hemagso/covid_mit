bb5f96ffd0f6464713848be15cbacebb7b174f64
Ubuntu <ubuntu@ip-172-31-39-207.us-east-2.compute.internal>
2020-04-20 17:08:11 +0000

    Updated

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.post('/api/store', (req, res) => {
     console.log("Got new item", req.body)
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        let dbo = db.db("covid");
-          dbo.collection("covid").insertOne(req.body, function(err, res) {
+        let dbo = db.db("COVID19");
+          dbo.collection("SURVEY").insertOne(req.body, function(err, res) {
             if (err) throw err;
             console.log("1 document inserted");
             db.close();
